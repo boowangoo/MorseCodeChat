8c5da1ae2a626afacfc111725a20f5d12df7cf10
boowangoo <bfwang@edu.uwaterloo.ca>
2017-08-28 04:57:47 -0400

    Roughly added key event FSM
    	modified:   public/js/chatCtrl.js

--- public/js/chatCtrl.js
+++ public/js/chatCtrl.js
@@ -1,7 +1,15 @@
 morseApp.controller('chatCtrl', ['$scope', '$interval', 
 function($scope, $interval) {
   $scope.test = 'hello';
-  $scope.chatMessage = '';
+  $scope.chatMessages = [
+    {
+      'msgID': 0, // this is the message object of the user
+      'name': $scope.user_name,
+      'message': []
+    }
+    // other users' message objects are given non-zero msgID
+  ];
+
   
   let socket = io();
   $scope.sendMessage = function(msg) {
@@ -12,15 +20,86 @@ function($scope, $interval) {
     });
   }
 
-  // let listener = new window.keypress.Listener();
-  // // console.log(listener);
-  // listener.simple_combo("space", function() {
-  //   let i = 0;
-  //   $interval(function() {
-  //     console.log("seconds passed:", i);
-  //     i++;
-  //   }, 1000);
-  // });
+  let listenPress = new $window.keypress.Listener();
+  // console.log(listenPress);
+  listenPress.simple_combo("space", function() {
+    let i = 0;
+    $interval(function() {
+      console.log("seconds passed:", i);
+      i++;
+    }, 1000);
+  });
+
+  let currTime = 0;
+  let currKeyEvent = listenDown;
+  let keyState = 0;
+  let ks = {
+    INIT: 0,
+    // L: Current key event Listener
+    CL_UP: 1, CL_DOWN: 2,
+    // O: Output to enqueue
+    O_DIT: 3, O_DAH: 4,
+    // (C|L|W)SPACE: (Component|Letter|Word) space
+    O_CSPACE: 5, O_LSPACE: 6, O_WSPACE: 7
+  }
+
+  let keyStateSM = function() {
+    let tu = $scope.timeUnit;
+    switch (keyState) {
+      case ks.INIT:
+        // (re)start timer
+        currKeyEvent = listenUp;
+        keyState = ks.CL_UP;
+        break;
+      case ks.CL_UP:
+        if (currTime < 1.5*tu) {
+          // enqueue dit
+          keyState = ks.O_DIT;
+        }
+        else if (currTime < 5*tu) {
+          // enqueue dah
+          keyState = ks.O_DAH;
+        }
+        else if (currTime > 4.5*tu) {
+          // kill timer
+          keyState = ks.INIT;
+        }
+        break;
+      case ks.CL_DOWN:
+        if (currTime < 1.5*tu) {
+          // enqueue cspace
+          keyState = ks.O_CSPACE;
+        }
+        else if (currTime < 5*tu) {
+          // enqueue lspace
+          keyState = ks.O_LSPACE;
+        }
+        else if (currTime < 10*tu) {
+          // enqueue wspace
+          keyState = ks.W_LSPACE;
+        }
+        else if (currTime > 9.5*tu) {
+          // kill timer
+          keyState = ks.INIT;
+        }
+        break;
+      case ks.O_DIT:
+      case ks.O_DAH:
+        // restart timer
+        currKeyEvent = listenDown;
+        keyState = ks.CL_DOWN;
+        break;
+      case ks.O_CSPACE:
+      case ks.O_LSPACE:
+      case ks.O_WSPACE:
+        // restart timer
+        currKeyEvent = listenUp;
+        keyState = ks.CL_UP;
+        break;
+      default:
+        break;
+    }
+  }
 
   $scope.keyPressCall = function(event) {
     let i = 0;
